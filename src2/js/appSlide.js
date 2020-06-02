10a8494f8107468a52fdac1a39646de1a996cc9f
Christiano0407 <placebo0704@gmail.com>
2020-06-02 06:36:22 -0500

    slide-4

---
+++ src2/js/appSlide.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $(`.btn1`).click(function(){
+        $(`.img1`).css("marginLeft", "0");
+    });
+    $(`.btn1`).click(function(){
+        $(`.img1`).css("marginLeft", "-20%");
+    });
+});
