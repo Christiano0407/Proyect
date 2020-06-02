b8533475656861dd5ab0368c4f815d7989db797e
Christiano0407 <placebo0704@gmail.com>
2020-06-02 07:18:38 -0500

    fin

--- src2/js/appSlide.js
+++ src2/js/appSlide.js
@@ -5,4 +5,19 @@ $(document).ready(function() {
     $(`.btn1`).click(function(){
         $(`.img1`).css("marginLeft", "-20%");
     });
+    $(`.btn2`).click(function(){
+        $(`.img1`).css("marginLeft", "-40%");
+    });
+    $(`.btn3`).click(function(){
+        $(`.img1`).css("marginLeft","-60%");
+    });
+    $(`.btn4`).click(function(){
+        $(`.img1`).css("marginLeft", "-80%");
+    });
+    $(`.btn5`).click(function(){
+        $(`.img1`).css("marginLeft", "-100%");
+    });
+    $(`.btn6`).click(function(){
+        $(`.img`).css("marginLeft", "-120%");
+    });
 });
