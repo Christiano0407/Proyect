d4bd2c86b5bd832b93b7f930481b32e8a1691b61
Christiano0407 <placebo0704@gmail.com>
2020-06-04 07:48:56 -0500

    parax-1

---
+++ src2/js/appParax.js
@@ -0,0 +1,28 @@
+const showMenu = (toggleId,navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*====== RELLAX =====*/
+var rellax = new Rellax('.parallax');
+
+
+/*====== ANIMATE GSAP ======*/
+/*Navbar*/
+gsap.from('.nav__logo', {opacity:0, duration: 3, delay: .5, y: 30, ease:'expo.out'});
+gsap.from('.nav__toggle', {opacity:0, duration: 3, delay: .7, y: 30, ease:'expo.out'});
+gsap.from('.nav__item', {opacity: 0, duration: 3, delay: .7, y: 35, ease:'expo.out', stagger: .2})
+
+/*Text*/
+gsap.from('.home__title', {opacity:0, duration: 3, delay: 1.3, y: 35, ease:'expo.out'});
+gsap.from('.home__subtitle', {opacity:0, duration: 3, delay: 1.1 , y: 35, ease:'expo.out'});
+
+/*Scroll*/
+gsap.from('.home__scroll', {opacity:0, duration: 3, delay: 1.5, y: 25, ease:'expo.out'});
