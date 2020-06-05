f26259e0956b9c308d5b7d190033b3aa333c0e1f
Christiano0407 <placebo0704@gmail.com>
2020-06-05 09:03:27 -0500

    carousel

--- src2/js/appSwipe.js
+++ src2/js/appSwipe.js
@@ -4,13 +4,18 @@ var swiper = new Swiper('.swiper-container', {
     centeredSlides: true,
     slidesPerView: 'auto',
     coverflowEffect: {
-      rotate: 50,
+      rotate: 20,
       stretch: 0,
-      depth: 100,
+      depth: 200,
       modifier: 1,
       slideShadows : true,
     },
-    pagination: {
+   /* pagination: {
       el: '.swiper-pagination',
-    },
+    },  */
+   loop:true, 
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
   });
