9ba5ec56532f1a21e34cabd317107952cd8685c7
Christiano0407 <placebo0704@gmail.com>
2020-06-12 12:15:00 -0500

    proyect-final

---
+++ src3/js/AppW.js
@@ -0,0 +1,42 @@
+const intro = document.querySelector(`.intro`);
+const video = intro.querySelector(`video`);
+const text = intro.querySelector(`h1`);
+
+const section = document.querySelector(`section`);
+const end = section.querySelector(`h1`);
+
+const controller = new ScrollMagic.Controller();
+
+const scene =  new ScrollMagic.Scene({
+    duration: 9000,
+    triggerElement:intro,
+    triggerHook: 0
+})
+
+.addIndicators()
+.setPin(intro)
+.addTo(controller);
+
+const textAnim = TweenMax.fromTo(text, 3, {opacity:1}, {opacity:0});
+
+let scene2 = new ScrollMagic.Scene({
+    duration:3000,
+    triggerElement:intro,
+    triggerHook:0
+})
+.setTween(textAnim)
+.addTo(controller)
+/* video Animation */
+let accelamount = 0.1;
+let scrollpos = 0;
+let delay = 0;
+
+scene.on(`update`, e => {
+    scrollpos = e.scrollPos / 1000;
+})
+setInterval(() => {
+    delay += (scrollpos - delay) * accelamount;
+    console.log(scrollpos, delay);
+
+    video.currentTime = delay;
+}, 33.3);
