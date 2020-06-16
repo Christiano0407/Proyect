501fc40c570bcbdbced5fe7836fd9798d4fea0f4
Christiano0407 <placebo0704@gmail.com>
2020-06-16 08:54:34 -0500

    fin

--- src3/js/AppLi.js
+++ src3/js/AppLi.js
@@ -1,7 +1,41 @@
-var myAnimation = new hoverEffect({
-parent: document.querySelector(`.distortion`),
-intensity: 0.2,
-image1:`../images/bw1.jpg`,
-image2:`../images/bw2.jpg`,
-displacementImage:`../images/heightMap.png`
+var hoverDistort = new hoverEffect({
+    parent: document.querySelector('.wrapper'),
+    intensity: 0.5,
+    image1: '../images/bw1.jpg',
+    image2: '../images/bw2.jpg',
+    displacementImage: '../images/strip.png'
+});
+
+var textWrapper = document.querySelector('.hero-title');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+$(document).ready(function() {
+
+$('.wrapper').hover(         
+      function () {
+      anime.timeline({loop: false})
+      .add({
+      targets: '.hero-title .letter',
+      translateX: [80,0],
+      translateZ: 0,
+      opacity: [0,1],
+      easing: "easeOutExpo",
+      duration: 1400,
+      delay: (el, i) => 100 + 40 * i
+      })
+      },
+
+      function () {
+      anime.timeline({loop: false})
+      .add({
+      targets: '.hero-title .letter',
+      translateX: [0,-80],
+      opacity: [1,0],
+      easing: "easeInExpo",
+      duration: 800,
+      delay: (el, i) => 40 * i
+      });         
+      }
+);
+            
 });
