5dc8a3d4423e74b6af90bfd5c0b99789d3fc40aa
Christiano0407 <placebo0704@gmail.com>
2020-06-16 08:38:03 -0500

    previous

--- src3/js/AppLi.js
+++ src3/js/AppLi.js
@@ -1,4 +1,7 @@
-new hoverEffect({
+var myAnimation = new hoverEffect({
 parent: document.querySelector(`.distortion`),
 intensity: 0.2,
+image1:`../images/bw1.jpg`,
+image2:`../images/bw2.jpg`,
+displacementImage:`../images/heightMap.png`
 });
